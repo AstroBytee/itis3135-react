c01a19e8ad73673808a58322f506e4d35f4e98d7
Ryan L <ryan.lac5@icloud.com>
2025-11-03 17:18:54 -0500

    added Header

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+    return <> {/* React Fragment */}
+        <header>
+            <h1>Ryan LaCroix's Regal Lynx || ITIS3135</h1>
+            <nav>
+                <a href="./">Home</a>
+                |
+                <a href="introduction.html">Introduction</a>
+                |
+                <a href="contract.html">Contract</a>
+            </nav>
+        </header>
+    </>
+}
